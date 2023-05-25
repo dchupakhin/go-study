5827ff443dcc487e68e305f45aa7f91cd384f26e
dchupakhin <dm.ch.dev@yandex.ru>
2023-05-26 00:30:41 +0300

    added json to req/resp

--- http/gorilla/gorilla.go
+++ http/gorilla/gorilla.go
@@ -1,22 +1,21 @@
 package gorilla
 
 import (
-	"github.com/dchupakhin/go-study/http/gorilla/server/handler"
 	"net/http"
+
+	"github.com/dchupakhin/go-study/http/gorilla/server/handler"
 
 	"github.com/gorilla/mux"
 )
 
-const MARK = "Gorilla"
-
 func StartServer(address string) error {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", handler.IndexHandler(MARK))
-	r.HandleFunc("/home", handler.HomeHandler(MARK))
-	r.HandleFunc("/hello/{name}", handler.HelloHandler(MARK))
-	r.HandleFunc(`/product/{id:\d+}`, handler.ProductHandler(MARK))
-	r.HandleFunc("/form", handler.FormHandler(MARK)).Methods("POST", "PUT")
+	r.HandleFunc("/", handler.IndexHandler)
+	r.HandleFunc("/home", handler.HomeHandler)
+	r.HandleFunc("/hello/{name}", handler.HelloHandler)
+	r.HandleFunc(`/product/{id:\d+}`, handler.ProductHandler)
+	r.HandleFunc("/form", handler.FormHandler).Methods("POST", "PUT")
 	r.NotFoundHandler = handler.NotFoundHandler{}
 
 	return http.ListenAndServe(address, r)
--- http/gorilla/server/handler/handler.go
+++ http/gorilla/server/handler/handler.go
@@ -1,68 +1,69 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+
+	"github.com/dchupakhin/go-study/model"
+	"github.com/gorilla/mux"
 )
 
 type NotFoundHandler struct {
 }
 
-func HomeHandler(msg string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Path: %s", r.URL.Path)
-		_, err := w.Write([]byte(fmt.Sprintf("Home >>> %s", msg)))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func IndexHandler(msg string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Path: %s", r.URL.Path)
-		_, err := w.Write([]byte(fmt.Sprintf("Index >>> %s", msg)))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func HelloHandler(msg string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := mux.Vars(r)["name"]
-		log.Printf("Path: %s", r.URL.Path)
-		_, err := w.Write([]byte(fmt.Sprintf("Hello, %s! >>> %s", name, msg)))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func ProductHandler(msg string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := mux.Vars(r)["id"]
-		log.Printf("Path: %s", r.URL.Path)
-		_, err := w.Write([]byte(fmt.Sprintf("Product id: %s >>> %s", name, msg)))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func FormHandler(msg string) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Path: %s", r.URL.Path)
-		_, err := w.Write([]byte(fmt.Sprintf("Form >>> %s", msg)))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
 func (NotFoundHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("404 >>> %s", r.URL.Path)
 	http.NotFound(w, r)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Path: %s", r.URL.Path)
+	_, err := w.Write([]byte("Home >>> Gorilla"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Path: %s", r.URL.Path)
+	_, err := w.Write([]byte("Index >>> Gorilla"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["name"]
+	log.Printf("Path: %s", r.URL.Path)
+	_, err := w.Write([]byte(fmt.Sprintf("Hello, %s! >>> Gorilla", name)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ProductHandler(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["id"]
+	log.Printf("Path: %s", r.URL.Path)
+	_, err := w.Write([]byte(fmt.Sprintf("Product id: %s >>> Gorilla", name)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Path: %s", r.URL.Path)
+	w.Header().Set("Content-Type", "application/json")
+	responseUser := model.User{ID: 1, Name: "Godev", Email: "sdf@sdf.ru", Password: "12345"}
+	encodeErr := json.NewEncoder(w).Encode(responseUser)
+	if encodeErr != nil {
+		panic(encodeErr)
+	}
+	var requestUser model.User
+	decodeErr := json.NewDecoder(r.Body).Decode(&requestUser)
+	if decodeErr != nil {
+		panic(decodeErr)
+	}
+	log.Printf("%#v", requestUser)
 }
